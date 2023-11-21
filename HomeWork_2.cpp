4d5c6b6105d3e04dc5bcaecbdcbba84e9a60af21
AleksSPB147 <aleks.racheev@gmail.com>
2023-11-21 22:27:56 +0300

    1,1

--- HomeWork_2.cpp
+++ HomeWork_2.cpp
@@ -17,7 +17,7 @@ int main()
 {
 	setlocale(LC_ALL, "RU");
 	//task_1();
-	//task_2();
+	task_2();
 	//task_3();
 	//task_4();
 	//task_5();
@@ -27,20 +27,64 @@ int main()
 }
 void task_1()
 {
-	float a, b, c, w;
+	float a, b, c, D;
 	printf("Введите коофициент: a ");
-	scanf_s("%f", &a);
+	cin >> a;
 	printf("Введите коофициент: в ");
-	scanf_s("%d", &b);
+	cin >> b;
 	printf("Введите коофициент: с ");
-	scanf_s("%f", &c);
-	w = sqrt(a);
-	//printf("%d", w);
-	cout << w;
+	cin >> c;
+	D = b * b - 4 * a * c;            // Находим дискриминант
+	cout << "Дискриминант = "<< D << endl;
+	if (D < 0)
+	{
+		cout << "Корней нет" << endl;
+	}
+
+	else if(D == 0)
+
+	{
+		cout << "x = " << - b / (2 * a)<< endl;
+	}
+	else
+	{
+		cout << " x1 = " << (-b + sqrt(D)) / (2 * a)<< endl;
+		cout << " x2 " << (-b - sqrt(D)) / (2 * a) << endl;
+
+	}
+
+
+
+
+
 
  }
 
 void task_2()
+{
+	int a, b, c;
+	printf("Введите первое число: ");
+	scanf_s("%d", &a);
+	printf("Введите второе число: ");
+	scanf_s("%d", &b);
+	printf("Введите третье число: ");
+	scanf_s("%d", &c);
+	if ((a > b) && (a < c) || (a < b) && (a > c))
+	{
+		cout << a;
+	}
+	else if ((b > a) && (b < c) || (b < a) && (b > c))
+	
+		cout << b;
+	
+	else 
+
+		cout << c;
+
+}
+
+
+void task_3()
 {
 	int a, b, c;
 	printf("Введите длину стороны а: ");
@@ -49,12 +93,9 @@ void task_2()
 	scanf_s("%d", &b);
 	printf("Введите длину стороны c: ");
 	scanf_s("%d", &c);
-		
+
 
 
 }
-
-
-
 
 
