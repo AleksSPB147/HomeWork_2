9fa1d475f19044e9d171d2a85bbbe2e4f2f56b6e
AleksSPB147 <aleks.racheev@gmail.com>
2023-11-21 16:01:34 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_2.cpp
@@ -0,0 +1,60 @@
+﻿
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+void task_1();
+void task_2();
+void task_3();
+void task_4();
+void task_5();
+
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	//task_1();
+	task_2();
+	//task_3();
+	//task_4();
+	//task_5();
+		
+
+
+}
+void task_1()
+{
+	float a, b, c, w;
+	printf("Введите коофициент: a ");
+	scanf_s("%f", &a);
+	printf("Введите коофициент: в ");
+	scanf_s("%d", &b);
+	printf("Введите коофициент: с ");
+	scanf_s("%f", &c);
+	w = sqrt(a);
+	//printf("%d", w);
+	cout << w;
+
+ }
+
+void task_2()
+{
+	int a, b, c;
+	printf("Введите длину стороны а: ");
+	scanf_s("%d", &a);
+	printf("Введите длину стороны b: ");
+	scanf_s("%d", &b);
+	printf("Введите длину стороны c: ");
+	scanf_s("%d", &c);
+		
+
+
+}
+
+
+
+
+
